330bc3ee1848d0384a8f26b23a7e51399885e1ec
Pavel Parshin <mail@pavelparshin.ru>
2020-05-29 11:02:40 +0300

    Initial Commit

---
+++ PersonalPageApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PersonalPageApp
+//
+//  Created by Pavel Parshin on 29.05.2020.
+//  Copyright © 2020 Pavel Parshin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
