c8d987ce47a8a6312ccdcbbadb3f3759a082fc64
Pavel Parshin <mail@pavelparshin.ru>
2020-05-31 18:51:17 +0300

    update WelcomePage and add color gradient

--- PersonalPageApp/ViewControllers/UserWelcomeViewController.swift
+++ PersonalPageApp/ViewControllers/UserWelcomeViewController.swift
@@ -21,27 +21,20 @@ class UserWelcomeViewController: UIViewController {
         
         guard let userName = self.userName else { return }
         welcomeLabel.text = "Welcome \(userName)!"
-        dayLabel.text = getGoodMessage()
+        dayLabel.text = "Good \(phaseOfTheDay().rawValue)!"
     }
     
     @IBAction func pressLogOutButton() {
     }
     
-    
-    //функция определяет текущий час и создаёт предложение проведения хорошего утра, дня, вечера, ночи
-    private func getGoodMessage() -> String {
-        var message = "Good "
-        
+    //Функция определяет фазу дня
+    private func phaseOfTheDay() -> PhaseOfTheDay {
         switch getCurrentHour() {
-        case 5...11:         message += "\(PhaseOfTheDay.morning)"
-        case 12...18:        message += "\(PhaseOfTheDay.day)"
-        case 19...21:        message += "\(PhaseOfTheDay.evening)"
-        case 22...23, 0...4: message += "\(PhaseOfTheDay.night)"
-        default:
-            message = "Have a nice time!"
+        case 5..<12:   return .morning
+        case 12..<18:  return .day
+        case 18..<22:  return .evening
+        default:       return .night
         }
-        
-        return message + "!"
     }
     
     //Определяем текущий час
@@ -50,6 +43,36 @@ class UserWelcomeViewController: UIViewController {
         let calendar = Calendar.current
         let hour = calendar.component(.hour, from: date)
         return hour
+    }
+    
+    //Градиентная заливка фона устанавливается в зависимости от фазы дня
+    func setGradientBackground() {
+    
+        var colorTop: CGColor
+        switch phaseOfTheDay() {
+        case .morning:
+            colorTop =  UIColor(red: 255.0/255.0, green: 255.0/255.0, blue: 0.0/255.0, alpha: 1.0).cgColor
+        case .day:
+            colorTop =  UIColor(red: 0.0/255.0, green: 0.0/255.0, blue: 255.0/255.0, alpha: 0.3).cgColor
+        case .evening:
+            colorTop =  UIColor(red: 50.0/255.0, green: 0.0/255.0, blue: 128.0/255.0, alpha: 1.0).cgColor
+        case .night:
+            colorTop =  UIColor(red: 0.0/255.0, green: 0.0/255.0, blue: 100.0/255.0, alpha: 1.0).cgColor
+        }
+        
+        let colorBottom = UIColor(red: 255.0/255.0, green: 255.0/255.0, blue: 255.0/255.0, alpha: 1.0).cgColor
+
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.colors = [colorTop, colorBottom]
+        gradientLayer.locations = [0.0, 1.0]
+        gradientLayer.frame = self.view.bounds
+
+        self.view.layer.insertSublayer(gradientLayer, at:0)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        setGradientBackground()
+        super.viewWillAppear(animated)
     }
     
 }
